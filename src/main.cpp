2aba10a1081264c7b2afb8369e27d5ba20fa1542
walotta <walotta@126.com>
2021-01-26 01:05:03 +0800

    debug for not call for ~cmd()

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 #include "CMD.h"
 
+CMD bookStoreManager;
 int main(){
-    CMD bookStoreManager;
     string cmd;
     while(getline(cin,cmd))
     {
@@ -14,7 +14,8 @@ int main(){
             bookStoreManager.run_command(cmd);
         }catch(const error& o)
         {
-            cout<<"Invalid\n";
+            //cout<<"Invalid\n";
+            cout<<o.get()<<endl;
         }
     }
     return 0;
