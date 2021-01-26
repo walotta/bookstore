9d5c77bcafd02bf5e9431f8e2156a72e8df25feb
walotta <walotta@126.com>
2021-01-26 23:09:12 +0800

    change to score

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,9 @@ int main(){
             bookStoreManager.run_command(cmd);
         }catch(const error& o)
         {
-            //cout<<"Invalid\n";
-            cout<<i<<endl;
-            cout<<o.get()<<endl;
+            cout<<"Invalid\n";
+            //cout<<i<<endl;
+            //cout<<o.get()<<endl;
         }
     }
     return 0;
